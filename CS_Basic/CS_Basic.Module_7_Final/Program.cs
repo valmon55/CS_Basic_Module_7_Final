bf4b9e6ee5ad666f0007ac3558ce5484f87f0207
valmon55 <fedor_ka@mail.ru>
2023-06-25 12:57:16 +0300

    Prepare

--- CS_Basic/CS_Basic.Module_7_Final/Program.cs
+++ CS_Basic/CS_Basic.Module_7_Final/Program.cs
@@ -1,5 +1,41 @@
 ﻿namespace CS_Basic.Module_7_Final
 {
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class Order<TDelivery,
+    TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        // ... Другие поля
+    }
     internal class Program
     {
         static void Main(string[] args)
