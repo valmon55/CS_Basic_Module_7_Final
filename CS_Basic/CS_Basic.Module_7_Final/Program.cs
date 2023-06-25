5b9a4db89768dc4a7c20f832f1c6a374ca588d19
valmon55 <fedor_ka@mail.ru>
2023-06-25 12:54:36 +0300

    Start

---
+++ CS_Basic/CS_Basic.Module_7_Final/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Basic.Module_7_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
